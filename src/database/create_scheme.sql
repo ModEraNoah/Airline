ad33fd0c4fce548fed486c26095a1991e27d3903
ModEraNoah <klauck02@outlook.com>
2023-11-19 21:02:20 +0100

    create scheme fix
    fixing minor mistakes that prevent the correct initiation

--- src/database/create_scheme.sql
+++ src/database/create_scheme.sql
@@ -2,27 +2,27 @@ CREATE TABLE airlines (
 id int CONSTRAINT airlines_id_unique UNIQUE,
 abbreviation VARCHAR(6) CONSTRAINT airlines_abr_pk PRIMARY KEY,
 name VARCHAR NOT NULL,
-rating float NOT NULL,
+rating float NOT NULL
 );
 
 CREATE TABLE continents (
 id int CONSTRAINT continents_id_pk PRIMARY KEY,
 name VARCHAR
-)
+);
 
 CREATE TABLE countries (
 id int CONSTRAINT countries_id_pk PRIMARY KEY,
 continent_id INT, 
-name VARCHAR
-CONSTRAINT continents_id_fk FOREIGN KEY(continent_id) REFERENCES continents(id),
+name VARCHAR,
+CONSTRAINT continents_id_fk FOREIGN KEY(continent_id) REFERENCES continents(id)
 );
 
 CREATE TABLE airports (
-id int CONSTRAINT airlines_id_unique UNIQUE,
-abbreviation VARCHAR(3) CONSTRAINT airlines_abr_pk PRIMARY KEY,
+id int CONSTRAINT ariport_id_unique UNIQUE,
+abbreviation VARCHAR(3) CONSTRAINT airport_abr_pk PRIMARY KEY,
 name VARCHAR NOT NULL,
 country_id INT,
-CONSTRAINT country_id_fk FOREIGN KEY(countrie_id) REFERENCES countries(id)
+CONSTRAINT country_id_fk FOREIGN KEY(country_id) REFERENCES countries(id)
 );
 
 CREATE Table flights (
@@ -33,7 +33,7 @@ dest_airport_id int,
 CONSTRAINT airline_id_fk FOREIGN KEY(airline_id) REFERENCES airlines(id),
 CONSTRAINT start_ap_id_fk FOREIGN KEY(start_airport_id) REFERENCES airports(id),
 CONSTRAINT dest_ap_id_fk FOREIGN KEY(dest_airport_id) REFERENCES airports(id)
-)
+);
 
 
 CREATE SEQUENCE airlines_sequence start 1 increment 1 OWNED BY airlines.id;
